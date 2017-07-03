6008eecce87b235e4a9fb1ed78f0b876a5d880f5
Sam Grundman <samuel@yodas.ws>
2017-07-02 21:36:12 -0400

    Switch from event-stream to through2

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-const es = require('event-stream'),
+const through = require('through2'),
   gutil = require('gulp-util');
 
 module.exports = function (opt) {
-  function gulpRmLines(file) {
-    if (file.isNull()) {
-      return this.emit('data', file);
-    }
+  return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
 
-    if (file.isStream()) {
-      return this.emit('error', new Error("gulp-delete-lines: Streaming not supported"));
-    }
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-rm-lines', 'Streaming not supported'));
+			return;
+		}
 
-    let str = file.contents.toString('utf8');
+    let str = file.contents.toString(enc);
     let newLines = [];
 
     const lines = str.split(/\r\n|\r|\n/g);
 
     for (let i=0; i<lines.length; i++) {
-      newLines.push(lines[i]);
+
+      let match = false;
+
       for (let j=0; j<opt.filters.length; j++) {
         if (lines[i].match(opt.filters[j])) {
-          newLines.pop();
+          match = true;
           break;
         }
+      }
+
+      if (!match) {
+        newLines.push(lines[i]);
       }
     }
 
     str = newLines.join('\n');
 
     file.contents = new Buffer(str);
-    this.emit('data', file);
-  }
+
+    this.push(file);
+
+		cb();
+  });
 
   return es.through(gulpRmLines);
 };
